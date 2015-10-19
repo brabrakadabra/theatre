13ad0c259fa9eae4364d6dbff92b43e51cd9d88d
brabrakadabra <brabrakadabra@gmail.com>
2015-10-19 19:30:03 +0200

    Second version of index.php

--- index.php
+++ index.php
@@ -15,15 +15,55 @@ and open the template in the editor.
             $db = new mysqli('localhost', 'root', 'root', 'test');
             $result = $db->query('SELECT * from actors');
             //print_r($result);
+		?>
+			<table>
+					<thead>
+						<tr>
+							<th>Full name
+							</th>
+                                                        <th>Date of birth
+							</th>
+                                                        <th>Description
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+					
+		<?php
+			
             if ($result) {
                 while ($row = $result->fetch_assoc()) {
-                    print_r($row);
-                    echo '<br>';
+                    //print_r($row);
+                   ?>
+                   <tr>
+							<td>
+                                                        <?php
+                                                            echo $row['first_name'].' '. $row['last_name'] ;
+                                                        ?>    
+							</td>
+							<td>
+                                                        <?php
+                                                               echo $row['birth_date'];
+                                                        ?> 
+							</td>
+							<td>
+                                                         <?php
+                                                               echo $row['description'];
+                                                        ?> 
+                                                        </td>
+						</tr>
+                  
+                     <?php
+                   
                 }
             } else {
                 echo "Wystąpił nieznany błąd";
             }                
         ?>
+                                        
+                                        </tbody>
+			</table>
     </body>
 </html>
  
