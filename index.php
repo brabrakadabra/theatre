d709bdefd2bb26d0a45b7a07194cbf5ce28d2a34
brabrakadabra <brabrakadabra@gmail.com>
2015-10-19 21:00:52 +0200

    Third version

--- index.php
+++ index.php
@@ -15,55 +15,48 @@ and open the template in the editor.
             $db = new mysqli('localhost', 'root', 'root', 'test');
             $result = $db->query('SELECT * from actors');
             //print_r($result);
-		?>
-			<table>
-					<thead>
-						<tr>
-							<th>Full name
-							</th>
-                                                        <th>Date of birth
-							</th>
-                                                        <th>Description
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						
-					
-		<?php
-			
+	?>
+            <table>
+		<thead>
+			<tr>
+                            <th>Full name
+                            </th>
+                            <th>Date of birth
+                            </th>
+                            <th>Description
+                            </th>
+			</tr>		
+	<?php
             if ($result) {
                 while ($row = $result->fetch_assoc()) {
                     //print_r($row);
-                   ?>
+        ?>
                    <tr>
-							<td>
-                                                        <?php
-                                                            echo $row['first_name'].' '. $row['last_name'] ;
-                                                        ?>    
-							</td>
-							<td>
-                                                        <?php
-                                                               echo $row['birth_date'];
-                                                        ?> 
-							</td>
-							<td>
-                                                         <?php
-                                                               echo $row['description'];
-                                                        ?> 
-                                                        </td>
-						</tr>
-                  
-                     <?php
-                   
-                }
+			<td>
+        <?php
+                        echo $row['first_name'].' '. $row['last_name'] ;
+        ?>    
+                        </td>
+			<td>
+        <?php
+                        echo $row['birth_date'];
+        ?> 
+			</td>
+			<td>
+        <?php
+                        echo $row['description'];
+        ?> 
+                        </td>
+                    </tr>
+        <?php
+                                                        }
             } else {
                 echo "Wystąpił nieznany błąd";
             }                
         ?>
                                         
-                                        </tbody>
-			</table>
+            </tbody>
+	</table>
     </body>
 </html>
  
